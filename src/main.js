1b18ff2355e437be2e334b240aa9fa362bfaca2d
mohamed abd elmoniem <mohammed.abd.elmoniiem@gmail.com>
2025-11-14 18:31:47 +0200

    add next 14 days

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import './style.css'
 
 var City = 'cairo'
+var DAY = 0;
 const API_KEY = "48ef6540816a43269e0172236251111";
 
 
@@ -15,7 +16,7 @@ const API_KEY = "48ef6540816a43269e0172236251111";
 // initail 
 // getWeatherData() 
 
-getFutureWeather(1)
+getFutureWeather(DAY)
 
 
 async function getWeatherData (city = "cairo"){
@@ -31,16 +32,114 @@ const API = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}
   display(result)
   backgroundCange(result.current.condition.text)
 }
+var nextDayDiv = document.querySelector('div.next-days');
 
+
+function next14dDay(forecastday,property = 'maxtemp_c'){
+
+  
+  if(property == 'maxtemp_c'){
+    nextDayDiv.innerHTML = `
+     <span class="position-absolute top-0 start-0 mt-1 ms-2 small-info"
+        >next 14 days predictions</span>
+    <div class="next-day-btn position-absolute">
+      <i class="fa fa-arrow-circle-right"></i>
+    </div>
+    `
+
+    document.querySelector('div.next-day-btn').addEventListener('click',nextDayWeather)
+  }
+  
+  
+  var width = nextDayDiv.getBoundingClientRect().width < 450 ? 450 :nextDayDiv.getBoundingClientRect().width;
+  var height = nextDayDiv.getBoundingClientRect().height;
+
+  console.log(nextDayDiv,width,height)
+  var svgGraph = document.createElement('div');
+  nextDayDiv.append(svgGraph)
+
+
+  svgGraph.classList.add('position-absolute', 'top-0' ,'start-0','end-0');
+  svgGraph.style.width = `${width}px`
+  // svgGraph.style.minWidth = `400px`
+
+
+  var arrMax = ``;
+  var arrPoints = ``;
+  var arrMAXdegree = ``;
+  var arrdate = ``
+
+  var min = 1000
+  var max = 0;
+
+  // for (var i = 0 ; i < forecastday.length ;i++){
+  //   if( forecastday[i].day[property] > max ) max = forecastday[i].day[property];
+  //   if( forecastday[i].day[property] < min  ) min = forecastday[i].day[property];
+  // }
+
+  console.log(min ,max,width,height)
+
+
+  
+   var firstPoint = [ (1/15)*width , (height*0.7  - ( forecastday[0].day[property] / 40)* height*0.4)]
+  for (var i = 0 ; i < forecastday.length ;i++){
+  
+    var   x = ((i+1)/15)*width ;
+
+      var  y  = (height*0.7  - ( forecastday[i].day[property] / 40)* height*0.4)  ;
+  
+    
+  
+
+    arrMax+= `L${x} ${y}`
+    arrPoints +=`<circle id="pointA" stroke=" ${DAY == i ? '#ffc400d8':'#b6b6b6d8'}" cx="${x}" cy="${y}" r="${DAY == i ? '5':'2'}" />`
+    arrMAXdegree += ` <text x="${x}" y="${y}" dx="0" dy="${property == 'maxtemp_c' ? '-10' : '17'}">${forecastday[i].day[property] } ْ</text>`;
+    arrdate += ` <text x="${x}" y="0" fill="${DAY == i ? '#ffc400d8':'#b6b6b6d8'}" dx="0" dy="${height-5}">${forecastday[i].date.match(/(?<=-)\d{1,2}-\d{1,2}/i)[0] }</text>`
+    
+
+  }
+
+  var str = `
+   <svg  class="" height="${height}" width="${width}" >
+  <path d=" M${ firstPoint[0]} ${ firstPoint[1]}  ${arrMax} "
+  style="fill:none;stroke: #b6b6b6d8;stroke-width:1" />
+
+   <g stroke="#b6b6b6d8" stroke-width="2" fill="#000000">
+    ${arrPoints}
+  </g>
+
+  <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
+    ${arrMAXdegree}
+    
+  </g>
+  <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
+    ${arrdate}
+    
+  </g>
+</svg> 
+  
+  `
+
+  svgGraph.innerHTML = str
+
+
+
+
+  
+}
 
 
 function display(result){
 
   
+  
 
-  var todayForecast = result['forecast']['forecastday'][0]['day'];
+  
 
-  var date = new Date(result['location']['localtime']);
+  var todayForecast = result['forecast']['forecastday'].at(DAY)['day'];
+
+  var date = new Date(`${ result['forecast']['forecastday'].at(DAY).date}`);
+  
 
   document.getElementById('city').innerText = result['location'].name;
 
@@ -71,12 +170,16 @@ function display(result){
   
 
   condition.innerHTML =`
-            <span class="position-absolute top-0 start-0 ms-4 display-6">now</span>
-            <p>${result['current']['temp_c']}ْ C</p>
-            <div class="image">
-              <img src="https:${result['current'].condition.icon}" alt="">
 
-              <span>${result['current'].condition.text}</span>
+              <span class="position-absolute top-0 start-0 mt-1 ms-2 small-info"
+              >average tempreture</span
+            >
+            
+            <p>${todayForecast['avgtemp_c']}ْ C</p>
+            <div class="image">
+              <img src="https:${todayForecast.condition.icon}" alt="">
+
+              <span>${todayForecast.condition.text}</span>
             </div>
             
   
@@ -93,7 +196,21 @@ function display(result){
   
 // display all data over the hours
 
-createTimes(result.forecast.forecastday[0].hour,result.forecast.forecastday[0].astro)
+createTimes(result.forecast.forecastday.at(DAY).hour,result.forecast.forecastday.at(DAY).astro)
+
+
+// display  predictions of the next 14 days
+next14dDay(result['forecast']['forecastday'],'maxtemp_c')
+next14dDay(result['forecast']['forecastday'],'mintemp_c')
+
+window.addEventListener('resize',()=>{
+
+
+// display  predictions of the next 14 days
+next14dDay(result['forecast']['forecastday'],'maxtemp_c')
+next14dDay(result['forecast']['forecastday'],'mintemp_c')
+
+})
  
 
  
@@ -126,9 +243,9 @@ function createWeatherInfo(name,value,icon){
 
 
 
-function getFutureWeather(day){
+function getFutureWeather(){
 
-  var API = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${City}&days=${day}&aqi=no`
+  var API = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${City}&days=${14}&aqi=no`
 
 
   var newPromes = new Promise((resolve,reject)=>{
@@ -226,7 +343,10 @@ function createTimes(allHoursData,astro){
 
 
   var times = document.querySelector('.times')
-  times.innerHTML = ""
+  times.innerHTML = `
+     <span class="position-absolute top-0 start-0 mt-1 ms-2 small-info "
+              >day predictions</span
+    `
 
   console.log(astro)
   var sunrise = +astro.sunrise.match(/^\d{2}(?=:)/i)[0];
@@ -311,6 +431,14 @@ function createTimes(allHoursData,astro){
 
 }
 
+// next day 
+
+function nextDayWeather(){
+   DAY = ++DAY >=14 ? 0:DAY;
+  getFutureWeather()
+}
+
+
 
 
 
@@ -389,7 +517,7 @@ ulList.addEventListener('click',function(eve){
 
   console.log(eve.target.innerText);
   City = eve.target.innerText
-  getFutureWeather(2)
+  getFutureWeather()
   
   ulList.innerHTML = ""
   searchInput.value = ""
