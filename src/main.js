83f44bfb76dc0cd6c7072f29d119cfb2e553cad7
mohamed abd elmoniem <mohammed.abd.elmoniiem@gmail.com>
2025-11-15 00:30:06 +0200

    add lots of changes

--- src/main.js
+++ src/main.js
@@ -10,117 +10,169 @@ import './style.css'
 var City = 'cairo'
 var DAY = 0;
 const API_KEY = "48ef6540816a43269e0172236251111";
+var weatherData = null;
 
 
 
 // initail 
 // getWeatherData() 
 
-getFutureWeather(DAY)
+getFutureWeather()
 
 
-async function getWeatherData (city = "cairo"){
+// async function getWeatherData (city = "cairo"){
 
-const API = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+// const API = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
 
 
-  var response = await fetch(API);
-  var result = await response.json()
-  console.log(result)
+//   var response = await fetch(API);
+//   var result = await response.json()
+//   console.log(result)
   
 
-  display(result)
-  backgroundCange(result.current.condition.text)
-}
+//   display(result)
+//   backgroundCange(result.current.condition.text)
+// }
 var nextDayDiv = document.querySelector('div.next-days');
 
 
-function next14dDay(forecastday,property = 'maxtemp_c'){
 
-  
-  if(property == 'maxtemp_c'){
-    nextDayDiv.innerHTML = `
-     <span class="position-absolute top-0 start-0 mt-1 ms-2 small-info"
-        >next 14 days predictions</span>
-    <div class="next-day-btn position-absolute">
-      <i class="fa fa-arrow-circle-right"></i>
-    </div>
-    `
 
-    document.querySelector('div.next-day-btn').addEventListener('click',nextDayWeather)
-  }
+function next14dDay(forecastday){
+
+
   
   
   var width = nextDayDiv.getBoundingClientRect().width < 450 ? 450 :nextDayDiv.getBoundingClientRect().width;
   var height = nextDayDiv.getBoundingClientRect().height;
 
-  console.log(nextDayDiv,width,height)
-  var svgGraph = document.createElement('div');
-  nextDayDiv.append(svgGraph)
-
-
-  svgGraph.classList.add('position-absolute', 'top-0' ,'start-0','end-0');
-  svgGraph.style.width = `${width}px`
-  // svgGraph.style.minWidth = `400px`
+  
 
 
   var arrMax = ``;
-  var arrPoints = ``;
+  var arrMaxPoints = ``;
   var arrMAXdegree = ``;
+
+  var arrMin = ``;
+  var arrMinPoints = ``;
+  var arrMindegree = ``;
+
   var arrdate = ``
 
-  var min = 1000
-  var max = 0;
+  
 
-  // for (var i = 0 ; i < forecastday.length ;i++){
-  //   if( forecastday[i].day[property] > max ) max = forecastday[i].day[property];
-  //   if( forecastday[i].day[property] < min  ) min = forecastday[i].day[property];
-  // }
+ 
 
-  console.log(min ,max,width,height)
+
 
 
   
-   var firstPoint = [ (1/15)*width , (height*0.7  - ( forecastday[0].day[property] / 40)* height*0.4)]
+   var firstMaxPoint = [ (1/15)*width , (height*0.7  - ( forecastday[0].day['maxtemp_c'] / 40)* height*0.4)];
+   var firstMinPoint = [ (1/15)*width , (height*0.7  - ( forecastday[0].day['mintemp_c'] / 40)* height*0.4)];
+
   for (var i = 0 ; i < forecastday.length ;i++){
   
     var   x = ((i+1)/15)*width ;
 
-      var  y  = (height*0.7  - ( forecastday[i].day[property] / 40)* height*0.4)  ;
+    var  yMax  = (height*0.7  - ( forecastday[i].day['maxtemp_c'] / 40)* height*0.4)  ;
+    var  yMin = (height*0.7  - ( forecastday[i].day['mintemp_c'] / 40)* height*0.4)  ;
+
   
     
   
+    // max line
+    arrMax+= `L${x} ${yMax}`
+    arrMaxPoints +=`<circle  class="${DAY == i ? "active":""}"  stroke="#ffffffd8" cx="${x}" cy="${yMax}" r="4" />`
+    arrMAXdegree += ` <text x="${x}" y="${yMax}" dx="0" dy="-10">${forecastday[i].day['maxtemp_c'] }ْ c</text>`;
+    // min line
 
-    arrMax+= `L${x} ${y}`
-    arrPoints +=`<circle id="pointA" stroke=" ${DAY == i ? '#ffc400d8':'#b6b6b6d8'}" cx="${x}" cy="${y}" r="${DAY == i ? '5':'2'}" />`
-    arrMAXdegree += ` <text x="${x}" y="${y}" dx="0" dy="${property == 'maxtemp_c' ? '-10' : '17'}">${forecastday[i].day[property] } ْ</text>`;
-    arrdate += ` <text x="${x}" y="0" fill="${DAY == i ? '#ffc400d8':'#b6b6b6d8'}" dx="0" dy="${height-5}">${forecastday[i].date.match(/(?<=-)\d{1,2}-\d{1,2}/i)[0] }</text>`
+     arrMin+= `L${x} ${yMin}`
+    arrMinPoints +=`<circle class="${DAY == i ? "active":""}"  stroke=" #ffffffd8" cx="${x}" cy="${yMin}" r="4" />`
+    arrMindegree += ` <text x="${x}" y="${yMin}" dx="0" dy="17">${forecastday[i].day['mintemp_c'] }ْ c</text>`;
+
+    // date
+    arrdate += ` <text x="${x}" y="0" fill="#b6b6b6d8" dx="0" dy="${height-40}">${forecastday[i].date.match(/(?<=-)\d{1,2}-\d{1,2}/i)[0] }</text>`
     
 
   }
 
-  var str = `
-   <svg  class="" height="${height}" width="${width}" >
-  <path d=" M${ firstPoint[0]} ${ firstPoint[1]}  ${arrMax} "
+//   var str = `
+//    <svg  class="" height="${height}" width="${width}" >
+//   <path d=" M${ firstPoint[0]} ${ firstPoint[1]}  ${arrMax} "
+//   style="fill:none;stroke: #b6b6b6d8;stroke-width:1" />
+
+//    <g stroke="#b6b6b6d8" stroke-width="2" fill="#000000">
+//     ${arrPoints}
+//   </g>
+
+//   <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
+//     ${arrMAXdegree}
+    
+//   </g>
+//   <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
+//     ${arrdate}
+    
+//   </g>
+// </svg> 
+  
+//   `
+
+  // svgGraph.innerHTML = str
+
+   var str2 = `
+  
+   
+  <path d=" M${ firstMaxPoint[0]} ${ firstMaxPoint[1]}  ${arrMax} "
   style="fill:none;stroke: #b6b6b6d8;stroke-width:1" />
 
-   <g stroke="#b6b6b6d8" stroke-width="2" fill="#000000">
-    ${arrPoints}
+   <g class="maxtemp" stroke="#b6b6b6d8" stroke-width="2" fill="#000000">
+    ${arrMaxPoints}
   </g>
 
   <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
     ${arrMAXdegree}
     
   </g>
+
+   <path d=" M${ firstMinPoint[0]} ${ firstMinPoint[1]}  ${arrMin} "
+  style="fill:none;stroke: #b6b6b6d8;stroke-width:1" />
+
+   <g class="mintemp" stroke="#b6b6b6d8" stroke-width="2" fill="#000000">
+    ${arrMinPoints}
+  </g>
+
+  <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
+    ${arrMindegree}
+    
+  </g>
   <g font-size="10" font-family="sans-serif" fill="white" text-anchor="middle">
     ${arrdate}
     
   </g>
-</svg> 
+ 
   
   `
 
-  svgGraph.innerHTML = str
+  document.querySelector('svg').innerHTML = str2
+
+
+  // coloring the today points
+
+  var currentPointMax = document.querySelector('svg g.maxtemp circle.active')
+
+  console.log(currentPointMax)
+  currentPointMax.setAttribute('stroke','red');
+  currentPointMax.setAttribute('r','6');
+  currentPointMax.classList.add('active')
+
+  var currentPointMin = document.querySelector('svg g.mintemp circle.active')
+
+  console.log(currentPointMin)
+  currentPointMin.setAttribute('stroke','red');
+  currentPointMin.setAttribute('r','6');
+  currentPointMin.classList.add('active')
+
+
 
 
 
@@ -130,9 +182,6 @@ function next14dDay(forecastday,property = 'maxtemp_c'){
 
 
 function display(result){
-
-  
-  
 
   
 
@@ -150,48 +199,27 @@ function display(result){
   
   document.getElementById('date').innerText =`${date.getDate() }  ${date.toLocaleDateString('en-US',{month:'long'})}`;
 
-  // remove 
-  document.querySelectorAll('div.remove').forEach(ele=>ele.remove())
+ 
 
   // overview about wearher over all day
 
-  createWeatherInfo('uv index',`${todayForecast["uv"]} `,'fa-solid fa-radiation')
+  document.querySelector('div.avg-temp>p').innerHTML = `${todayForecast['maxtemp_c']}/${todayForecast['mintemp_c']}  C`;
+
+  document.querySelector('div.avg-wind>p').innerHTML = `${todayForecast["maxwind_kph"]} kph`;
+
+  document.querySelector('div.avg-humidity>p').innerHTML = `${todayForecast.avghumidity}%`;
+
+  document.querySelector('div.avg-uv>p').innerHTML = `${todayForecast.uv}`;
 
 
-   createWeatherInfo('humidity',`${todayForecast.avghumidity}%`,"fa-solid fa-droplet" );
-
-   createWeatherInfo('wind',`${todayForecast["maxwind_kph"]} kph`,'fa-solid fa-wind')
-
-// temperature info
-  createWeatherInfo('temperature', `${todayForecast['maxtemp_c']}/${todayForecast['mintemp_c']}  C`  ,'fa-solid fa-sun',true);
 
 
   var condition = document.querySelector('div.condition');
+  condition.querySelector('p').innerHTML = `${todayForecast['avgtemp_c']}ْ C`;
+  condition.querySelector('img').src = `https:${todayForecast.condition.icon}`;
+
+  condition.querySelector('span.text').innerHTML = `${todayForecast.condition.text}`
   
-
-  condition.innerHTML =`
-
-              <span class="position-absolute top-0 start-0 mt-1 ms-2 small-info"
-              >average tempreture</span
-            >
-            
-            <p>${todayForecast['avgtemp_c']}ْ C</p>
-            <div class="image">
-              <img src="https:${todayForecast.condition.icon}" alt="">
-
-              <span>${todayForecast.condition.text}</span>
-            </div>
-            
-  
-  `
-
-
-
-  
-
- 
-
- 
 
   
 // display all data over the hours
@@ -199,40 +227,6 @@ function display(result){
 createTimes(result.forecast.forecastday.at(DAY).hour,result.forecast.forecastday.at(DAY).astro)
 
 
-// display  predictions of the next 14 days
-next14dDay(result['forecast']['forecastday'],'maxtemp_c')
-next14dDay(result['forecast']['forecastday'],'mintemp_c')
-
-window.addEventListener('resize',()=>{
-
-
-// display  predictions of the next 14 days
-next14dDay(result['forecast']['forecastday'],'maxtemp_c')
-next14dDay(result['forecast']['forecastday'],'mintemp_c')
-
-})
- 
-
- 
-
- 
-
-
-}
-
-function createWeatherInfo(name,value,icon){
-  var div = document.createElement('div');
-  div.classList.add('info','remove')
-
-  div.innerHTML = `
-            <div class="">
-              <i class="${icon}"></i>
-              <span>${name}</span>
-            </div>
-
-            <p >${value}</p>
-  `
-  document.querySelector('div.condition').after(div);
 }
 
 
@@ -265,9 +259,17 @@ function getFutureWeather(){
 
   })
 
-  newPromes.then(respone=>{
-    console.log(respone)
-    display(respone)
+  newPromes.then(result=>{
+    weatherData = result;
+    
+    display(result);
+
+   
+
+    next14dDay(result['forecast']['forecastday'])
+    
+
+    
     
     
   
@@ -276,6 +278,132 @@ function getFutureWeather(){
 
 }
 
+window.addEventListener('resize',()=>{
+
+    // display  predictions of the next 14 days
+    next14dDay(weatherData['forecast']['forecastday'])
+   
+})
+
+
+ document.querySelector('div.next-day-btn>i.fa-arrow-circle-right').addEventListener('click',(e)=>{
+      DAY = ++DAY >=14 ? 0:DAY;
+      display(weatherData);
+      nextDayPoints()
+
+    });
+document.querySelector('div.next-day-btn>i.fa-arrow-circle-left').addEventListener('click',(e)=>{
+      DAY = --DAY < 0 ? 13:DAY;
+      display(weatherData);
+      previoustDayPoints()
+
+    });
+
+
+
+function nextDayPoints(){
+
+  var currentPointMax = document.querySelector('svg g.maxtemp circle.active');
+  currentPointMax.setAttribute('stroke','white');
+  currentPointMax.setAttribute('r','3');
+  currentPointMax.classList.remove('active')
+
+ 
+
+  
+  // max temp
+  if(currentPointMax.nextElementSibling != undefined){
+   
+      currentPointMax.nextElementSibling.setAttribute('stroke','red');
+      currentPointMax.nextElementSibling.setAttribute('r','6');
+      currentPointMax.nextElementSibling.classList.add('active')
+  }else{
+
+    var currentPointMax = document.querySelector('svg g.maxtemp circle:first-of-type')
+
+    currentPointMax.setAttribute('stroke','red');
+    currentPointMax.setAttribute('r','6');
+    currentPointMax.classList.add('active')
+
+  }
+
+
+  // min temp
+  var currentPointMin = document.querySelector('svg g.mintemp circle.active');
+  currentPointMin.setAttribute('stroke','white');
+  currentPointMin.setAttribute('r','3');
+  currentPointMin.classList.remove('active')
+
+
+  if(currentPointMin.nextElementSibling != undefined){
+   
+      currentPointMin.nextElementSibling.setAttribute('stroke','red');
+      currentPointMin.nextElementSibling.setAttribute('r','6');
+      currentPointMin.nextElementSibling.classList.add('active')
+  }else{
+
+    var currentPointMin = document.querySelector('svg g.mintemp circle:first-of-type')
+
+    currentPointMin.setAttribute('stroke','red');
+    currentPointMin.setAttribute('r','6');
+    currentPointMin.classList.add('active')
+
+  }
+
+}
+
+
+function previoustDayPoints(){
+
+  var currentPointMax = document.querySelector('svg g.maxtemp circle.active');
+  currentPointMax.setAttribute('stroke','white');
+  currentPointMax.setAttribute('r','3');
+  currentPointMax.classList.remove('active')
+
+ 
+
+  
+  // max temp
+  if(currentPointMax.previousElementSibling != undefined){
+   
+      currentPointMax.previousElementSibling.setAttribute('stroke','red');
+      currentPointMax.previousElementSibling.setAttribute('r','6');
+      currentPointMax.previousElementSibling.classList.add('active')
+  }else{
+
+    var currentPointMax = document.querySelector('svg g.maxtemp circle:last-of-type')
+
+    currentPointMax.setAttribute('stroke','red');
+    currentPointMax.setAttribute('r','6');
+    currentPointMax.classList.add('active')
+
+  }
+
+
+  // min temp
+  var currentPointMin = document.querySelector('svg g.mintemp circle.active');
+  currentPointMin.setAttribute('stroke','white');
+  currentPointMin.setAttribute('r','3');
+  currentPointMin.classList.remove('active')
+
+
+  if(currentPointMin.previousElementSibling != undefined){
+   
+      currentPointMin.previousElementSibling.setAttribute('stroke','red');
+      currentPointMin.previousElementSibling.setAttribute('r','6');
+      currentPointMin.previousElementSibling.classList.add('active')
+  }else{
+
+    var currentPointMin = document.querySelector('svg g.mintemp circle:last-of-type')
+
+    currentPointMin.setAttribute('stroke','red');
+    currentPointMin.setAttribute('r','6');
+    currentPointMin.classList.add('active')
+
+  }
+
+
+}
 // getFutureWeather(2)
 
 
@@ -358,7 +486,6 @@ function createTimes(allHoursData,astro){
   
   for(var i = 0 ; i< 24 ;i++){
 
-    // var rotationZ= ((i+6.5)/24)*360 ;
   
 
    
@@ -367,10 +494,7 @@ function createTimes(allHoursData,astro){
     time.setAttribute('time',i)
     time.classList.add('time');
 
-    // if(new Date().getHours() == i){
-    //   time.classList.add('active')
-    // }
-
+  
     console.log(sunrise,sunset)
    
     // sunrise
@@ -404,15 +528,12 @@ function createTimes(allHoursData,astro){
 
     var divIamge= document.createElement('div');
     divIamge.classList.add('img')
-    // divIamge.style.transform = `rotateZ(-${rotationZ}deg)`
     divIamge.innerHTML = `<img src="https:${allHoursData[i].condition.icon}" alt="" />`
 
     var pTemp = document.createElement('p')
-    // pTemp.style.transform = `rotateZ(-${rotationZ}deg)`
     pTemp.innerHTML = `${allHoursData[i].temp_c}ْ`;
 
     var pClock = document.createElement('p');
-    // pClock.style.transform = `rotateZ(-${rotationZ}deg)`
     pClock.innerHTML = `${i%12 == 0 ? 12 : i%12}${i< 12 ? 'am':'pm'} `
 
     time.append(pClock,divIamge,pTemp)
@@ -430,38 +551,6 @@ function createTimes(allHoursData,astro){
 }
 
 }
-
-// next day 
-
-function nextDayWeather(){
-   DAY = ++DAY >=14 ? 0:DAY;
-  getFutureWeather()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -495,11 +584,9 @@ function searchCity(q){
   request.addEventListener('readystatechange',(eve)=>{
     if(request.readyState == 4){
       var result = JSON.parse(request.response) 
-      console.log(result
-      )
+      console.log(result)
 
       
-
       var str = ``
 
       for(var i = 0 ; i <result.length ; i++ ){
